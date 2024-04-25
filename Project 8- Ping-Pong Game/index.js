253e1b78c7b7712a8d33dc1f60b4582d4dbb3be9
arijit-s98 <sarijit2172@gmail.com>
2024-04-25 19:47:32 +0530

    Added paddle collision

--- Project 8- Ping-Pong Game/index.js
+++ Project 8- Ping-Pong Game/index.js
@@ -24,14 +24,23 @@ document.addEventListener("DOMContentLoaded", () => {
         // if(ballX > 600-20 || ballX <= 0) dx *= -1;
         // if(ballY > 300-20 || ballY <= 0) dy *= -1;
 
-        if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1;
-        if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
+        if(ballX < paddle.offsetLeft + paddle.offsetWidth &&
+            ballY > paddle.offsetTop && 
+            ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight){
+            dx*= -1;
+        }
+        //collision of ball and paddle
+
+        if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1;  //change x-direction
+        if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1; //change y-direction
+
     });
 
 
     let paddleY = 0;
     let dPy = 5;
     document.addEventListener("keydown", (event) => {
+        event.preventDefault();  // prevents the execution of the default event behaviour
         if(event.keyCode == 38 && paddleY > 0) {
             //up arrow
             paddleY += (-1)* dPy;
